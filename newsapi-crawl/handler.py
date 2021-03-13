85cff73fcee85387ebffefc264ca6a0b0f5bc4af
Louis Guitton <louisguitton93@gmail.com>
2021-03-13 20:39:12 +0100

    Update serverless function

--- newsapi-crawl/handler.py
+++ newsapi-crawl/handler.py
@@ -3,41 +3,15 @@ import json
 import logging
 import os
 from typing import Dict
-import urllib
 
 import boto3
-from newsapi import NewsApiClient   
+from newsapi import NewsApiClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-newsapi_key = os.environ['NEWSAPI_KEY']
 
-
-def get_domains(newsapi: NewsApiClient) -> Dict[str, str]:
-    french_sources = {}
-    for category in [
-        "business",
-        "entertainment",
-        "general",
-        "health",
-        "science",
-        "sports",
-        "technology",
-    ]:
-        top_headlines = newsapi.get_top_headlines(
-            category=category, language="fr", country="fr"
-        )
-        french_sources.update(
-            {
-                a["source"]["name"]: urllib.parse.urlparse(a["url"]).netloc.lstrip(
-                    "www."
-                )
-                for a in top_headlines["articles"]
-            }
-        )
-
-    return french_sources
+newsapi_key = os.environ["NEWSAPI_KEY"]
 
 
 def get_news(
@@ -54,23 +28,39 @@ def get_news(
     return all_articles["articles"]
 
 
+def main(date_at: datetime):
+    newsapi = NewsApiClient(api_key=newsapi_key)
+
+    # save 1 API call by loading from file
+    # NewsAPI developer account is rate limited to 100 requests per day
+    with open("./sources.json", "r") as fh:
+        french_sources = json.load(fh)
+
+    articles = get_news(newsapi, french_sources, date_at=date_at)
+
+    if len(articles):
+        s3_bucket = "articles-louisguitton"
+        s3_key = f"newsapi/{date_at.strftime('%Y-%m-%d/%H')}/articles.json"
+        logger.info(f"Writing to s3://{s3_bucket}/{s3_key}")
+        s3 = boto3.resource("s3")
+        s3object = s3.Object(s3_bucket, s3_key)
+        jsonline_body = "\n".join([json.dumps(a) for a in articles])
+        s3object.put(Body=(bytes(jsonline_body.encode("UTF-8"))))
+    else:
+        logger.info("No articles to write to S3")
+
+
 def run(event, context):
-    # https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents-expressions.html
-    # https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents.html
-    time = datetime.datetime.strptime(event["time"], "%Y-%m-%dT%H:%M:%SZ")
+    """Lambda function handler that will run on schedule.
+    
+    Ref:
+        - https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents.html
+    """
+    # When the function is triggered at time 2021-03-12T10:05:00, we want the news from the last hour
+    time = datetime.datetime.strptime(
+        event["time"], "%Y-%m-%dT%H:%M:%SZ"
+    ) - datetime.timedelta(hours=1)
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(time))
 
-    newsapi = NewsApiClient(api_key=newsapi_key)
-    french_sources = get_domains(newsapi)
-    articles = get_news(newsapi, french_sources, date_at=time)
-
-    s3_bucket = "articles-louisguitton"
-    s3_key = f"newsapi/{time.strftime('%Y-%m-%d/%H')}/articles.json"
-    logger.info(f"Writing to s3:/{s3_bucket}/{s3_key}")
-    s3 = boto3.resource('s3')
-    s3object = s3.Object(s3_bucket, s3_key)
-
-    s3object.put(
-        Body=(bytes(json.dumps(articles).encode('UTF-8')))
-    )
+    main(time)
